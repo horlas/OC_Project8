1e74553170d4a6e01d97f2c3fc1d2202ff59a6ad
horlas <aurelia.gourbere@gmail.com>
2018-09-18 21:34:28 +0200

    initialisation projet 8

---
+++ pur_beurre/quality/urls.py
@@ -0,0 +1,10 @@
+from . import views
+from django.urls import path
+from django.conf.urls import url
+
+app_name = "quality"
+
+urlpatterns = [
+
+
+]
---
+++ pur_beurre/quality/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.db import transaction, IntegrityError
+
+def index(request):
+   
+    return render(request, 'quality/index.html')
