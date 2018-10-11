23b6e58eeec6bfed9181d79aa0a2d5c1ca1d6267
horlas <aurelia.gourbere@gmail.com>
2018-10-12 00:24:06 +0200

    dumpdata

--- pur_beurre/prod_settings.py
+++ pur_beurre/prod_settings.py
@@ -20,5 +20,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 db_from_env = dj_database_url.config()
 
-DATABASES['default'] = dj_database_url.config()
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
